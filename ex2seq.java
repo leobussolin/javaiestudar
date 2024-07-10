1a6c9d0a6d7ff839a92aee209f34aae789e22930
leobussolin <97371421+leobussolin@users.noreply.github.com>
2024-07-09 21:43:22 -0300

    Add files via upload

---
+++ ex2seq.java
@@ -0,0 +1,22 @@
+package curso_java;
+import java.util.math;
+import java.util.Locale;
+import java.util.Scanner;
+public class ex2seq {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//declarando tipos de variável
+		double R, area;
+		//inserindo valores
+		R = sc.nextFloat();
+		//cálculo da área
+		area = 3.14159 * Math.pow(R, 2);
+		//para seperação decimal no padrão dos Estados Unidos
+		Locale.setDefault(Locale.US);
+		
+		System.out.printf("A=%.4f%n", area);
+		
+	}
+
+}

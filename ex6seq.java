fe10639195b27ae5389ce1d4390e59fb6531b9f1
Leonardo Bussolin <97371421+leobussolin@users.noreply.github.com>
2024-07-10 23:58:48 -0300

    Add files via upload

---
+++ ex6seq.java
@@ -0,0 +1,35 @@
+package curso_java;
+
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.Locale;
+
+public class ex6seq {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		//declaração de variáveis
+		double A, B, C, triRet, circ, trap, quad, ret, pi = 3.14159;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		A = sc.nextDouble();
+		B = sc.nextDouble();
+		C = sc.nextDouble();
+		
+		triRet = A * C;
+		circ = pi * (Math.pow(C, 2));
+		trap = (A + B)* C / 2;
+		quad = Math.pow(B, 2);
+		ret = A * B;
+		
+		System.out.printf("TRIANGULO: %.3f%n", triRet);
+		System.out.printf("CIRCULO: %.3f%n", circ);
+		System.out.printf("TRAPEZIO: %.3f%n", trap);
+		System.out.printf("QUADRADO: %.3f%n", quad);
+		System.out.printf("RETANGULO: %.3f%n", ret);
+		
+		sc.close();
+	}
+
+}

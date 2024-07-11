5f79f96bd1de8d82122a0c32c7a01b1bfe2d25b6
Leonardo Bussolin <97371421+leobussolin@users.noreply.github.com>
2024-07-10 23:43:45 -0300

    Update ex5seq.java

--- ex5seq.java
+++ ex5seq.java
@@ -6,12 +6,13 @@ import java.util.Locale;
 public class ex5seq {
 
 	public static void main(String[] args) {
-		
+		//padrao EUA decimais
 		Locale.setDefault(Locale.US);
+		//declaração de variáveis
 		Scanner sc = new Scanner(System.in);
 		int p1, cd1, p2, cd2;
 		double val1,val2, compra;
-		
+		//inserção de dados
 		cd1 = sc.nextInt();
 		p1 = sc.nextInt();
 		val1 = sc.nextDouble();
@@ -19,9 +20,9 @@ public class ex5seq {
 		cd2 = sc.nextInt();
 		p2 = sc.nextInt();
 		val2 = sc.nextDouble();
-		
+		//cálculo do valor da compra
 		compra = (p1 * val1) + (p2 * val2);
-		
+		//mensagem informando valor a ser pago
 		System.out.printf("VALOR A PAGAR: R$ %.2f%n", compra);
 		sc.close();
 	}

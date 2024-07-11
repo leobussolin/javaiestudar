d70bccccbffb877eded6f0f99e0ff540311ef9e9
Leonardo Bussolin <97371421+leobussolin@users.noreply.github.com>
2024-07-11 00:01:36 -0300

    Update ex6seq.java

--- ex6seq.java
+++ ex6seq.java
@@ -10,19 +10,19 @@ public class ex6seq {
 		Locale.setDefault(Locale.US);
 		//declaração de variáveis
 		double A, B, C, triRet, circ, trap, quad, ret, pi = 3.14159;
-		
+		//inserção de dados
 		Scanner sc = new Scanner(System.in);
 		
 		A = sc.nextDouble();
 		B = sc.nextDouble();
 		C = sc.nextDouble();
-		
-		triRet = A * C;
+		//cálculos
+		triRet = A * C / 2;
 		circ = pi * (Math.pow(C, 2));
 		trap = (A + B)* C / 2;
 		quad = Math.pow(B, 2);
 		ret = A * B;
-		
+		//mostrando os resultados
 		System.out.printf("TRIANGULO: %.3f%n", triRet);
 		System.out.printf("CIRCULO: %.3f%n", circ);
 		System.out.printf("TRAPEZIO: %.3f%n", trap);
